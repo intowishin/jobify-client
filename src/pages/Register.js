b0a6b47cfa34582ead62df23926c3f5ad2f10d3a
Kamila Kowalczyk <kamila.kowalczyk@icloud.com>
2022-07-11 23:17:19 -0400

    FormRow Component

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,40 +1,60 @@
-import {useState, useEffect} from 'react'
-import {Logo} from '../components/'
-import Wrapper from '../assets/wrappers/RegisterPage'
+import { useState, useEffect } from "react";
+import { Logo, FormRow } from "../components/";
+import Wrapper from "../assets/wrappers/RegisterPage";
 
 const initialState = {
-  name: '',
-  email: '',
-  password: '',
+  name: "",
+  email: "",
+  password: "",
   isMember: true,
-}
+};
 
 const Register = () => {
-  const [values, setValues] = useState(initialState)
+  const [values, setValues] = useState(initialState);
   //global state and useNavigate
 
   const handleChange = (e) => {
-    console.log(e.target)
-  }
+    console.log(e.target);
+  };
 
   const onSubmit = (e) => {
-    e.preventDefault()
-    console.log(e.target)
-  }
+    e.preventDefault();
+    console.log(e.target);
+  };
   return (
-    <Wrapper className='full-page'>
-    <form className='form' onSubmit={onSubmit}>
-<Logo />
-<h3>Login</h3>
-   
-{/* name input */}
-<div className='form-row'>
-  <label htmlFor='name' className='form-label'>Name</label>
-  <input type='text' value={values.name} name='name' onChange={handleChange} className='form-input'></input>
-</div>
-<button type='submit' className='btn btn-block'>submit</button>
- </form>
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+
+        {/* name input */}
+        <FormRow
+          type="text"
+          name="name"
+          value={values.name}
+          handleChange={handleChange}
+        />
+
+        {/* email input */}
+        <FormRow
+          type="email"
+          name="email"
+          value={values.email}
+          handleChange={handleChange}
+        />
+
+        {/* password input */}
+        <FormRow
+          type="password"
+          name="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+      </form>
     </Wrapper>
-  )
-}
-export default Register
+  );
+};
+export default Register;
